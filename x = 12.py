b02631df47adcc7dc712a57649fa75f8b87a561c
Tiviant01 <ianrelox10@gmail.com>
2025-01-18 11:50:31 +0800

    TW25

---
+++ x = 12.py
@@ -0,0 +1,10 @@
+num = int(input ('Enter a number: '))
+
+if num == 12:
+    for i in range (1,5):
+        print("Hooray")
+elif num == 24:
+    for i in range (1,10):
+        print("Hooray")
+else:
+        print("Aguy")
